63803a80f535a932074c90ae35e726409dd22a0b
Sergio Jiménez <sjr.social@gmail.com>
2026-06-09 15:51:40 +0200

    refactor: optimize font loading, update background assets to WebP, and streamline layout styling

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,26 @@
-import { defineConfig } from "astro/config";
+import { defineConfig, fontProviders } from "astro/config";
 
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
+  fonts: [
+    {
+      provider: fontProviders.local(),
+      name: "Onest",
+      cssVariable: "--font-onest",
+      options: {
+        variants: [
+          {
+            weight: "100 900",
+            style: "normal",
+            src: ["./src/assets/fonts/Onest.woff2"],
+          },
+        ],
+      },
+    },
+  ],
 });
